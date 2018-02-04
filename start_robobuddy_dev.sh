27d86dc0ec926d8e13dd0d347cb6028c95e526be
berners <matthewbernerslee@gmail.com>
2018-02-04 00:23:04 +0000

    adding start robobuddy (dev machine) script

---
+++ start_robobuddy_dev.sh
@@ -0,0 +1,17 @@
+#! /bin/bash
+
+vboxmanage startvm robobuddy --type headless
+done=0
+echo "Waiting for robobuddy to awaken"
+while [ $done -ne 1 ]; do
+  ping -c 1 robobuddy &>/dev/null
+  if [ $? -eq 0 ]; then
+    let done=1
+    printf "robobuddy is alive!\n"
+  else
+    printf "%c" "."
+  fi
+done
+
+ssh berners@robobuddy
+#VBoxManage controlvm robobuddy acpipowerbutton
