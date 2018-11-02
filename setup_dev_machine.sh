bc53205159f67b1450a658ea7b381baf667b4c42
Berners <matthewbernerslee@gmail.com>
2018-11-02 09:11:56 +0000

    x

--- setup_dev_machine.sh
+++ setup_dev_machine.sh
@@ -8,7 +8,7 @@
 #TODO make name@machine color change scripted
 
 function configure_bashrc {
-  source_bashrc_additions="source ~/src/dev_setup/dev_machine_bashrc_additions"
+  source_bashrc_additions="source ~/src/dev_setup/bashrc_dev_machine_additions"
   if [ `grep -c "$source_bashrc_additions" ~/.bashrc` -eq 0 ]; then
     echo "$source_bashrc_additions" >> ~/.bashrc
   fi
