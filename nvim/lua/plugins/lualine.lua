89c17aaec060251bacf666ef078e960c3c9fd585
Matt Berners Berners-Lee <matthew.berners-lee@multiverse.io>
2024-03-01 16:12:48 +0000

    full filepath for inactive splits

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -23,6 +23,20 @@ return {
           }
         }
       }
+      inactive_sections = {
+        lualine_c = {
+          {
+            'filename',
+            file_status = false,    -- Displays file status (readonly status, modified status)
+            newfile_status = false, -- Display new file status (new file means no write after created)
+            path = 1,               -- 0: Just the filename
+            -- 1: Relative path
+            -- 2: Absolute path
+            -- 3: Absolute path, with tilde as the home directory
+            -- 4: Filename and parent dir, with tilde as the home directory
+          }
+        }
+      }
     }
     )
   end,
