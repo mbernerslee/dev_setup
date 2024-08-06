8499df7d7cffcda8a0bdcc541031f30e4cbdd862
Matt Berners Berners-Lee <matthew.berners-lee@multiverse.io>
2024-08-06 08:57:54 +0100

    adding markdown preview plugin

--- nvim/lua/plugins/claude.lua
+++ nvim/lua/plugins/claude.lua
@@ -1,7 +1,7 @@
 return {
   "https://github.com/pasky/claude.vim.git",
   config = function()
-    local claude_api_key = os.getenv("CLAUDE_API_KEY")
+    local claude_api_key = os.getenv("ANTHROPIC_API_KEY")
     vim.g.claude_api_key = claude_api_key
   end,
 }
