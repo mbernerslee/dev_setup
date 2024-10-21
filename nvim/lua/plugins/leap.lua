bdcc616faebbcf264d80ef818cfc7e8713df654e
Matthew Berners-Lee <matthew.berners-lee@Matthew-BernersLee-Mac.local>
2024-10-21 16:11:07 +0100

    fixing the m1 setup script (mostly)

--- nvim/lua/plugins/leap.lua
+++ nvim/lua/plugins/leap.lua
@@ -1,7 +1,8 @@
 return {
   "https://github.com/ggandor/leap.nvim.git",
   dependencies = {
-    "git@github.com:tpope/vim-repeat.git",
+    --"git@github.com:tpope/vim-repeat.git",
+    "https://github.com/tpope/vim-repeat.git"
   },
   config = function()
     require("leap").create_default_mappings()
